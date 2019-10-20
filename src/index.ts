272f3bf51643c76c738f6f44def7535e28813f0b
Leon <leon.lafa@gmail.com>
2019-10-20 20:31:47 +0100

    0.1.1

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ module.exports = async (
 
       if (Data)
         console.log(
-          chalk.bgHex("#3FBB90").bold.hex("#1F344A")(" SUCCESS ") + chalk.hex("#3FBB90")(` ${Data.length} ${message}`)
+          chalk.bgHex("#3FBB90").bold.hex("#1F344A")(" SUCCESS ") +
+            chalk.hex("#3FBB90")(` ${Data.length} ${message}`)
         );
 
       const source = Data.map((coin: any) => {
